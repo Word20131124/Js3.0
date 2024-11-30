8a6646afbd33b2e78b9a030c550aea18e6ed5954
Word20131124 <miron.mir2913@gmail.com>
2024-11-30 17:14:28 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+//console.log(Math.round(6.7));
+//console.log(Math.ceil(1.9));
+//console.log(Math.floor(8.3));
+//console.log(Math.random());
+//console.log(Math.sqrt(157));
+//console.log(Math.min(7,9,1,4));
+//console.log(Math.max(6, 5, 4, 9, 100000));
+
+
+//const age = 19;
+
+//if(age < 19) {
+//    console.log('извените но вам пора делать домашку');
+//}else if (age > 18){
+//console.log('добро жаловать на нас')
+//}
+
+
+alert ('Sizda hozir random sonlar chiqishni boshledi');
+
+alert ('Random orqali chiqqan son: 22');
+
+confirm ('biz random sonni 2 ga kopaytirmoqchi va bolmoqchimiz !');
+
+alert (`Random orqali chiqqan son: 22 edi
+biz uni 2 ga kopaytirib 44 sonini chiqardik,
+va shu sonni 2 ga bolib: 11 sonni ciqardik,
+va shu sonni 3 ga bolgandagi qoldiq: 1 sonini chiqardik
+`);
+
+    alert ('Sizda hozir random sonlar chiqishni boshledi');
+
+    alert ('Random orqali chiqqan son: 55');
+
+    confirm ('biz random sonni 2 ga kopaytirmoqchi va bolmoqchimiz !');
+
+    alert (`Random orqali chiqqan son: 55 edi
+biz uni 2 ga kopaytirib 110 sonini chiqardik,
+va shu sonni 2 ga bolib: 27.5 sonni ciqardik,
+va shu sonni 3 ga bolgandagi qoldiq: 1 sonini chiqardik
+`);
